1cf60aed35d546782fd6e3ff88beedb37c08eb94
Czarek Tomczak <czarek.tomczak@gmail.com>
2013-02-22 12:29:42 -0800

    Fixed tabs to spaces in pdo.php

--- var/pdo.php
+++ var/pdo.php
@@ -6,78 +6,78 @@
 
 function PDO_Connect($dsn, $user="", $password="")
 {
-	global $PDO;
-	$PDO = new PDO($dsn, $user, $password);
-	$PDO->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
+    global $PDO;
+    $PDO = new PDO($dsn, $user, $password);
+    $PDO->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
 }
 function PDO_FetchOne($query, $params=null)
 {
-	global $PDO;
-	if (isset($params)) {
-		$stmt = $PDO->prepare($query);
-		$stmt->execute($params);
-	} else {
-		$stmt = $PDO->query($query);
-	}
-	$row = $stmt->fetch(PDO::FETCH_NUM);
-	if ($row) {
-		return $row[0];
-	} else {
-		return false;
-	}
+    global $PDO;
+    if (isset($params)) {
+        $stmt = $PDO->prepare($query);
+        $stmt->execute($params);
+    } else {
+        $stmt = $PDO->query($query);
+    }
+    $row = $stmt->fetch(PDO::FETCH_NUM);
+    if ($row) {
+        return $row[0];
+    } else {
+        return false;
+    }
 }
 function PDO_FetchRow($query, $params=null)
 {
-	global $PDO;
-	if (isset($params)) {
-		$stmt = $PDO->prepare($query);
-		$stmt->execute($params);
-	} else {
-		$stmt = $PDO->query($query);
-	}
-	return $stmt->fetch(PDO::FETCH_ASSOC);
+    global $PDO;
+    if (isset($params)) {
+        $stmt = $PDO->prepare($query);
+        $stmt->execute($params);
+    } else {
+        $stmt = $PDO->query($query);
+    }
+    return $stmt->fetch(PDO::FETCH_ASSOC);
 }
 function PDO_FetchAll($query, $params=null)
 {
-	global $PDO;
-	if (isset($params)) {
-		$stmt = $PDO->prepare($query);
-		$stmt->execute($params);
-	} else {
-		$stmt = $PDO->query($query);
-	}
-	return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    global $PDO;
+    if (isset($params)) {
+        $stmt = $PDO->prepare($query);
+        $stmt->execute($params);
+    } else {
+        $stmt = $PDO->query($query);
+    }
+    return $stmt->fetchAll(PDO::FETCH_ASSOC);
 }
 function PDO_FetchAssoc($query, $params=null)
 {
-	global $PDO;
-	if (isset($params)) {
-		$stmt = $PDO->prepare($query);
-		$stmt->execute($params);
-	} else {
-		$stmt = $PDO->query($query);
-	}
-	$rows = $stmt->fetchAll(PDO::FETCH_NUM);
-	$assoc = array();
-	foreach ($rows as $row) {
-		$assoc[$row[0]] = $row[1];
-	}
-	return $assoc;
+    global $PDO;
+    if (isset($params)) {
+        $stmt = $PDO->prepare($query);
+        $stmt->execute($params);
+    } else {
+        $stmt = $PDO->query($query);
+    }
+    $rows = $stmt->fetchAll(PDO::FETCH_NUM);
+    $assoc = array();
+    foreach ($rows as $row) {
+        $assoc[$row[0]] = $row[1];
+    }
+    return $assoc;
 }
 function PDO_Execute($query, $params=null)
 {
-	global $PDO;
-	if (isset($params)) {
-		$stmt = $PDO->prepare($query);
-		$stmt->execute($params);
-	} else {
-		$PDO->query($query);
-	}
+    global $PDO;
+    if (isset($params)) {
+        $stmt = $PDO->prepare($query);
+        $stmt->execute($params);
+    } else {
+        $PDO->query($query);
+    }
 }
 function PDO_InsertId()
 {
-	global $PDO;
-	return $PDO->lastInsertId();
+    global $PDO;
+    return $PDO->lastInsertId();
 }
 
 ?>
