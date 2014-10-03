ff2f939d8c6194e53ee6448c31c593b9c4bd6141
Czarek Tomczak <czarek.tomczak@gmail.com>
2014-10-03 18:03:10 +0200

    Updated php.ini, error reporting was disabled completely.
    Updated system-command.php - may han on XP (Issue 124).

--- phpdesktop-chrome/www/system-command.php
+++ phpdesktop-chrome/www/system-command.php
@@ -4,7 +4,29 @@
 
 <h1>system() command</h1>
 
+<pre style="background:#ddd">
+// system() command may hang on Windows XP for some programs,
+// wmic.exe is one of them. See Issue 124:
+// https://code.google.com/p/phpdesktop/issues/detail?id=124
+
+ob_start();
+$ret = system("C:\Windows\System32\wbem\wmic.exe path win32_computersystemproduct get uuid");
+$output = ob_get_contents();
+ob_end_clean();
+
+if ($ret !== false) {
+    echo "Command executed successfully.&lt;br&gt;";
+    echo "Output: $output";
+} else {
+    echo "Command failed.";
+}
+</pre>
+
 <?php
+
+// system() command may hang on Windows XP for some programs,
+// wmic.exe is one of them. See Issue 124:
+// https://code.google.com/p/phpdesktop/issues/detail?id=124
 
 ob_start();
 $ret = system("C:\Windows\System32\wbem\wmic.exe path win32_computersystemproduct get uuid");
@@ -20,3 +42,4 @@ if ($ret !== false) {
 
 
 ?>
+
