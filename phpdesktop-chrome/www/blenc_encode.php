aee4b77769743162b99035cdca2f161ef7f34b9e
Czarek Tomczak <czarek.tomczak@gmail.com>
2014-11-24 11:36:54 +0100

    Updated BLENC encoder example.

--- phpdesktop-chrome/www/blenc_encode.php
+++ phpdesktop-chrome/www/blenc_encode.php
@@ -1,4 +1,5 @@
 <?php
+define("BLENC_ENCRYPTION_KEY", "ChangeThisToSomethingElse");
 error_reporting(-1);
 $source_code = file_get_contents("blenc_myscript.php");
 // The encoded source passed to blenc_encrypt() cannot contain
@@ -8,16 +9,17 @@ $source_code = file_get_contents("blenc_myscript.php");
 $source_code = preg_replace('#^<'.'\?php\s+#', '', $source_code);
 $source_code = preg_replace('#\s+\?>\s*$#', '', $source_code);
 if (preg_match('#<'.'\?#', $source_code) 
-        || preg_match('#\?>#', $source_code)) {
+        || preg_match('#\?'.'>#', $source_code)) {
     print("Script to be encoded can only contain PHP code.");
     print(" Only a single php opening tag at the beginning of file");
     print(" and a single php closing tag at the end of file are allowed.");
     print(" This is a limitation as of BENC encoder 1.1.4b.");
     exit();
 }
-$redist_key = blenc_encrypt($source_code, "blenc_myscript_encoded.php");
+$redist_key = blenc_encrypt($source_code, "blenc_myscript_encoded.php",
+                            BLENC_ENCRYPTION_KEY);
 $key_file = ini_get('blenc.key_file');
-file_put_contents($key_file, $redist_key, FILE_APPEND);
+file_put_contents($key_file, $redist_key);
 print("DONE. See");
 print(" <a href='blenc_myscript_encoded.php'>blenc_myscript_encoded.php</a>");
 ?>
