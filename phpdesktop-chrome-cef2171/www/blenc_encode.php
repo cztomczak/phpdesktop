0d7ec5da30a8f31253b1f88cc85c4df5143d6af5
Czarek Tomczak <czarek.tomczak@gmail.com>
2014-11-28 11:56:29 +0100

    Updated BLENC encoder example.

--- phpdesktop-chrome-cef2171/www/blenc_encode.php
+++ phpdesktop-chrome-cef2171/www/blenc_encode.php
@@ -1,6 +1,20 @@
 <?php
 define("BLENC_ENCRYPTION_KEY", "ChangeThisToSomethingElse");
 error_reporting(-1);
+print("<a href='javascript:history.go(-1)'>Go back</a><br>");
+if (!extension_loaded("blenc")) {
+    $link = "https://code.google.com/p/phpdesktop/wiki/SourceCodeProtection".
+            "#BLENC_encoder";
+    printf("ERROR: blenc extension not loaded.<br><br>
+            BLENC encoder is a PECL extension that permits to protect 
+            PHP source scripts.
+            This extension is not distributed by default with 
+            phpdesktop binaries.
+            See instructions on how to use BLENC encoder with 
+            phpdesktop on the SourceCodeProtection wiki page:<br>
+            <a href='%s'>%s</a>", $link, $link);
+    exit();
+}
 $source_code = file_get_contents("blenc_myscript.php");
 // The encoded source passed to blenc_encrypt() cannot contain
 // any php tags. We are removing php tags at the beginning and
--- phpdesktop-chrome/www/blenc_myscript.php
+++ phpdesktop-chrome/www/blenc_myscript.php
@@ -1,3 +1,4 @@
 <?php
+print("<a href='javascript:history.go(-1)'>Go back</a><br>");
 print("Printing a secret string: XuXuXaaa");
 ?>
