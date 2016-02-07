02289eba9a3a3462b3acda0a7e8695c72dadc400
cztomczak <cztomczak@users.noreply.github.com>
2016-02-07 13:24:38 +0100

    Delete CEF branch 1750

--- phpdesktop-chrome-cef1750/www/env-variables.php
+++
@@ -1,9 +0,0 @@
-<a href="index.php">Go back to index</a>
-| <a href="<?php echo $_SERVER["REQUEST_URI"];?>">Refresh</a>
-
-<title>Environment variables</title>
-<h1>Environment variables</h1>
-
-<pre style="background:#ddd;">
-<?php print_r($_ENV); ?>
-</pre>
