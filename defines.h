ed76c7cd1265b8bdb9ac049b5c3434a9701692b3
cztomczak <cztomczak@users.noreply.github.com>
2024-11-05 23:04:43 +0100

    Set minimum Windows version supported to Windows 10.

--- defines.h
+++ defines.h
@@ -4,10 +4,12 @@
 
 #pragma once
 
-#define WINVER          0x0500
-#define _WIN32_WINNT    0x0501
+#define WINVER          _WIN32_WINNT_WIN10
+#define _WIN32_WINNT    _WIN32_WINNT_WIN10
 #define _WIN32_IE       _WIN32_IE_IE60SP2
 #define _RICHEDIT_VER   0x0200
+
+#define _AMD64_ 1
 
 // Win messages
 #define WM_TRAY_MESSAGE (WM_USER + 1)
