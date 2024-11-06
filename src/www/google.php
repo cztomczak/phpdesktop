1936afd7ac151147d0d478bbe5f482b11acfbc52
cztomczak <cztomczak@users.noreply.github.com>
2024-11-06 12:26:32 +0100

    Remove src/ from master. See appropriate branches for sources.

--- src/www/google.php
+++
@@ -1,8 +0,0 @@
-<style type="text/css">@import url("style.css");</style>
-<a href="index.php">Go back to index</a>
-| <a href="<?php echo $_SERVER["REQUEST_URI"];?>">Refresh</a>
-
-<title>Google</title>
-<h1>Google</h1>
-
-<a href="https://www.google.com/">https://www.google.com/</a>
